d59afcf90168d41c6c867d8c50b53524c625b570
vincent <vincent.fxz@gmail.com>
2013-06-08 19:53:50 +0800

    update

--- src/main/java/edu/pku/ammeterclient/AmmeterClient.java
+++ src/main/java/edu/pku/ammeterclient/AmmeterClient.java
@@ -58,34 +58,42 @@ public class AmmeterClient {
 				
 				int totalBytesRcvd=0;
 				int bytesRcvd;
-				byte[] received = new byte[34];
+
+                socketWriter.write(0);
+                socketWriter.flush();
+
+                while (true){
+                    byte[] received = new byte[1024];
+                    socketReader.read(received);
+                    System.out.println(AmmeterClient.bytesToHexString(received));
+                }
+
 				
-				while(totalBytesRcvd<34){
-					if((bytesRcvd=socketReader.read(received, totalBytesRcvd, 34-totalBytesRcvd))==-1){
-						throw new SocketException("Connection closed prematurely");
-					}
-					totalBytesRcvd+=bytesRcvd;
-					
-					System.out.println("Receved: "+AmmeterClient.bytesToHexString(received));
-					System.out.println(totalBytesRcvd);
-				}
-				socketWriter.write(0);
-				socketWriter.flush();
-				
-				socketWriter.write(b);
-				socketWriter.flush();
-				received = new byte[34];
-				
-				while(totalBytesRcvd<34){
-					if((bytesRcvd=socketReader.read(received, totalBytesRcvd, 34-totalBytesRcvd))==-1){
-						throw new SocketException("Connection closed prematurely");
-					}
-					totalBytesRcvd+=bytesRcvd;
-					
-					System.out.println("Receved: "+new String(AmmeterClient.bytesToHexString(received)));
-					System.out.println(totalBytesRcvd);
-				}
-				System.out.println("Receved: "+new String(AmmeterClient.bytesToHexString(received)));
+//				while(totalBytesRcvd<34){
+//					if((bytesRcvd=socketReader.read(received, totalBytesRcvd, 34-totalBytesRcvd))==-1){
+//						throw new SocketException("Connection closed prematurely");
+//					}
+//					totalBytesRcvd+=bytesRcvd;
+//
+//					System.out.println("Receved: "+AmmeterClient.bytesToHexString(received));
+//					System.out.println(totalBytesRcvd);
+//				}
+////
+//
+//				socketWriter.write(b);
+//				socketWriter.flush();
+//				received = new byte[34];
+//
+//				while(totalBytesRcvd<34){
+//					if((bytesRcvd=socketReader.read(received, totalBytesRcvd, 34-totalBytesRcvd))==-1){
+//						throw new SocketException("Connection closed prematurely");
+//					}
+//					totalBytesRcvd+=bytesRcvd;
+//
+//					System.out.println("Receved: "+new String(AmmeterClient.bytesToHexString(received)));
+//					System.out.println(totalBytesRcvd);
+//				}
+//				System.out.println("Receved: "+new String(AmmeterClient.bytesToHexString(received)));
 //				clientSocket.close();
 			}
 			
@@ -119,25 +127,6 @@ public class AmmeterClient {
         //try in idea
 
 		new AmmeterClient().test();
-//		byte[] b = new byte[1024];
-////		68 75 06 00 08 08 12 68 11 04 33 33 33 33 4E 16
-//		b[0] = (byte) 0x68;
-//		b[1] = (byte) 0x75;
-//		b[2] = (byte) 0x06;
-//		b[3] = (byte) 0x00;
-//		b[4] = (byte) 0x08;
-//		b[5] = (byte) 0x08;
-//		b[6] = (byte) 0x12;
-//		b[7] = (byte) 0x68;
-//		b[8] = (byte) 0x11;
-//		b[9] = (byte) 0x04;
-//		b[10] = (byte) 0x33;
-//		b[11] = (byte) 0x33;
-//		b[12] = (byte) 0x33;
-//		b[13] = (byte) 0x33;
-//		b[14] = (byte) 0x4E;
-//		b[15] = (byte) 0x16;
-//		System.out.println(AmmeterClient.bytesToHexString(b));
 
 	}
 
